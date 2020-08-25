773bdf9a58f2d23b0af9ca63ce2666aaaab5a464
MikeDerWolf <mihai.lupu777@gmail.com>
2020-08-25 12:28:19 +0300

    Done logger and methods

--- src/main/java/MAIN/Main2.java
+++ src/main/java/MAIN/Main2.java
@@ -5,15 +5,19 @@ import Entity.Employee;
 import Entity.JobCategory;
 
 import javax.persistence.*;
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main2 {
 
     private static EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("my-persistence-unit");
     private static EntityManager entityManager = entityManagerFactory.createEntityManager();
     private static EntityTransaction entityTransaction = entityManager.getTransaction();
+    private static Logger logger = Logger.getLogger(Main2.class.getName());
 
     public static void main(String[] args) {
 
@@ -83,23 +87,96 @@ public class Main2 {
         entityTransaction.commit();
         entityManager.close();*/
 
-        //findEmployeeById(1);
-
-        //listEmployeesByName();
-
-        //listEmployeesBySalary();
-
-        findEmployeesByDepartmentId(2);
-
-        //updateProductPrice(2, 5.62);
-
-        //deleteEmployee(9);
-
-        //findDepartmentById(1);
+        Scanner in = new Scanner(System.in);
+        options();
+        int option = in.nextInt();
 
 
+        while(true) {
 
-        entityManager.close();
+            switch(option) {
+                case 1:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idEmployee = in.nextInt();
+                    logger.log(Level.INFO, "Afisare angajat dupa ID");
+                    findEmployeeById(idEmployee);
+                    break;
+                case 2:
+                    logger.log(Level.INFO, "Afisare angajati");
+                    listEmployees();
+                    break;
+                case 3:
+                    logger.log(Level.INFO, "Afisare angajati sortati dupa nume");
+                    listEmployeesByName();
+                    break;
+                case 4:
+                    logger.log(Level.INFO, "Afisare angajati sortati dupa salariu descrescator");
+                    listEmployeesBySalary();
+                    break;
+                case 5:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idDepartment = in.nextInt();
+                    logger.log(Level.INFO, "Afisare angajati dintr-un departament");
+                    findEmployeesByDepartmentId(idDepartment);
+                    break;
+                case 6:
+                    logger.log(Level.INFO, "Afisare departamente");
+                    listDepartments();
+                    break;
+                case 7:
+                    logger.log(Level.INFO, "Afisare categorii job-uri");
+                    listJobCategories();
+                    break;
+                case 8:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idEmployeeDelete = in.nextInt();
+                    logger.log(Level.INFO, "Stergere angajat");
+                    deleteEmployee(idEmployeeDelete);
+                    break;
+                case 9:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idDepartmentDelete = in.nextInt();
+                    logger.log(Level.INFO, "Stergere departament");
+                    deleteDepartment(idDepartmentDelete);
+                    break;
+                case 10:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idJobCategoryDelete = in.nextInt();
+                    logger.log(Level.INFO, "Stergere categorie job");
+                    deleteEmployee(idJobCategoryDelete);
+                    break;
+                case 11:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idEmployeeUpdate = in.nextInt();
+                    logger.log(Level.INFO, "Actualizare angajat");
+                    updateEmployee(idEmployeeUpdate);
+                    break;
+                case 12:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idDepartmentUpdate = in.nextInt();
+                    logger.log(Level.INFO, "Actualizare departament");
+                    updateDepartment(idDepartmentUpdate);
+                    break;
+                case 13:
+                    System.out.println("Introduceti ID-ul: ");
+                    int idJobCategoryUpdate = in.nextInt();
+                    logger.log(Level.INFO, "Actualizare categorie job");
+                    updateJobCategory(idJobCategoryUpdate);
+                    break;
+                case 0:
+                    entityManager.close();
+                    return;
+                default:
+                    logger.log(Level.WARNING, "Incorrect value!");
+            }
+
+            options();
+            option = in.nextInt();
+
+        }
+
+
+
     }
 
     public static void findEmployeeById(int id){
@@ -111,7 +188,7 @@ public class Main2 {
             System.out.println(e.toString());
         }
         else{
-            System.out.println("Could not find employee!");
+            logger.log(Level.INFO, "Could not find employee!");
         }
 
     }
@@ -128,7 +205,7 @@ public class Main2 {
             }
         }
         else{
-            System.out.println("Could not find any records!");
+            logger.log(Level.INFO, "Could not find any records!");
         }
     }
 
@@ -144,7 +221,7 @@ public class Main2 {
             }
         }
         else{
-            System.out.println("Could not find any records!");
+            logger.log(Level.INFO, "Could not find any records!");
         }
     }
 
@@ -160,7 +237,7 @@ public class Main2 {
             }
         }
         else{
-            System.out.println("Could not find any records!");
+            logger.log(Level.INFO, "Could not find any records!");
         }
     }
 
@@ -177,38 +254,44 @@ public class Main2 {
             }
         }
         else{
-            System.out.println("Could not find any records!");
+            logger.log(Level.INFO, "Could not find any records!");
         }
 
     }
 
-    /*public static void updateProductPrice(int id, double price){
+    public static void listDepartments(){
 
-        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("my-persistence-unit");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
+        Query query = entityManager.createQuery("SELECT d FROM Department d");
+        List<Department> resultList = query.getResultList();
 
-        Product p = entityManager.find(Product.class, id);
-
-        if(p != null) {
-
-            entityTransaction.begin();
-            p.setPrice(price);
-            entityTransaction.commit();
+        if(resultList.size() != 0) {
+            for (Department d : resultList) {
+                System.out.println();
+                System.out.println(d.toString());
+            }
         }
         else{
-            System.out.println("Could not find product!");
+            logger.log(Level.INFO, "Could not find any records!");
         }
+    }
 
-        entityManager.close();
+    public static void listJobCategories(){
 
+        Query query = entityManager.createQuery("SELECT j FROM JobCategory j");
+        List<JobCategory> resultList = query.getResultList();
+
+        if(resultList.size() != 0) {
+            for (JobCategory j : resultList) {
+                System.out.println();
+                System.out.println(j.toString());
+            }
+        }
+        else{
+            logger.log(Level.INFO, "Could not find any records!");
+        }
     }
 
     public static void deleteEmployee(int id){
-
-        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("my-persistence-unit");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
 
         Employee e = entityManager.find(Employee.class, id);
 
@@ -217,24 +300,237 @@ public class Main2 {
             entityTransaction.begin();
             entityManager.remove(e);
             entityTransaction.commit();
+            logger.log(Level.INFO, "Stergere efectuata!");
         }
         else{
-            System.out.println("Could not find employee!");
+            logger.log(Level.INFO, "Could not find employee!");
         }
     }
 
-    public static void findDepartmentById(int id){
-
-        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("my-persistence-unit");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+    public static void deleteDepartment(int id){
 
         Department d = entityManager.find(Department.class, id);
 
         if(d != null) {
-            System.out.println(d.toString());
+
+            entityTransaction.begin();
+            entityManager.remove(d);
+            entityTransaction.commit();
+            logger.log(Level.INFO, "Stergere efectuata!");
         }
         else{
-            System.out.println("Could not find department!");
+            logger.log(Level.INFO, "Could not find department!");
         }
-    }*/
+    }
+
+    public static void deleteJobCategory(int id){
+
+        JobCategory j = entityManager.find(JobCategory.class, id);
+
+        if(j != null) {
+
+            entityTransaction.begin();
+            entityManager.remove(j);
+            entityTransaction.commit();
+            logger.log(Level.INFO, "Stergere efectuata!");
+        }
+        else{
+            logger.log(Level.INFO, "Could not find job category!");
+        }
+    }
+
+    public static void updateDepartment(int id){
+
+        Department d = entityManager.find(Department.class, id);
+
+        if(d != null) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("New name for department: ");
+                String name = scanner.nextLine();
+                entityTransaction.begin();
+                d.setName(name);
+                entityTransaction.commit();
+                scanner.close();
+                logger.log(Level.INFO, "Actualizare efectuata!");
+            } catch (Exception e){
+                entityTransaction.rollback();
+                logger.log(Level.SEVERE, "Failed operation!");
+            }
+        }
+        else{
+            logger.log(Level.INFO, "Could not find department!");
+        }
+
+    }
+
+    public static void updateJobCategory(int id){
+
+        JobCategory j = entityManager.find(JobCategory.class, id);
+
+        if(j != null) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("New name for job category: ");
+                String name = scanner.nextLine();
+                entityTransaction.begin();
+                j.setName(name);
+                entityTransaction.commit();
+                scanner.close();
+                logger.log(Level.INFO, "Actualizare efectuata!");
+            } catch (Exception e){
+                entityTransaction.rollback();
+                logger.log(Level.SEVERE, "Failed operation!");
+            }
+        }
+        else{
+            logger.log(Level.INFO, "Could not find job category!");
+        }
+
+    }
+
+    public static void updateEmployee(int id){
+
+        Employee e = entityManager.find(Employee.class, id);
+
+        if(e != null) {
+
+            Scanner scanner = new Scanner(System.in);
+            employeeOptions();
+            int option = scanner.nextInt();
+            boolean ok = true;
+
+            while(ok) {
+
+                switch(option) {
+                    case 1:
+                        try {
+                            System.out.println("New last name: ");
+                            String lastName = scanner.nextLine();
+                            entityTransaction.begin();
+                            e.setLastName(lastName);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 2:
+                        try {
+                            System.out.println("New first name: ");
+                            String firstName = scanner.nextLine();
+                            entityTransaction.begin();
+                            e.setFirstName(firstName);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 3:
+                        try {
+                            System.out.println("Is manager? ");
+                            boolean isManager = scanner.nextBoolean();
+                            entityTransaction.begin();
+                            e.setManager(isManager);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 4:
+                        try {
+                            System.out.println("New address: ");
+                            String address = scanner.nextLine();
+                            entityTransaction.begin();
+                            e.setAddress(address);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 5:
+                        try {
+                            System.out.println("New number of children: ");
+                            int noChildren = scanner.nextInt();
+                            entityTransaction.begin();
+                            e.setNoChildren(noChildren);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 6:
+                        try {
+                            System.out.println("New salary: ");
+                            double salary = scanner.nextDouble();
+                            entityTransaction.begin();
+                            e.setSalary(salary);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 7:
+                        try {
+                            System.out.println("Has driving license? ");
+                            boolean hasDrivingLicense = scanner.nextBoolean();
+                            entityTransaction.begin();
+                            e.setHasDrivingLicense(hasDrivingLicense);
+                            entityTransaction.commit();
+                            logger.log(Level.INFO, "Actualizare efectuata!");
+                        }catch (Exception ex){
+                            entityTransaction.rollback();
+                            logger.log(Level.SEVERE, "Failed operation!");
+                        }
+                        break;
+                    case 0:
+                        ok = false;
+                        break;
+                    default:
+                        logger.log(Level.WARNING, "Incorrect value!");
+                    }
+                    if(ok) {
+                        employeeOptions();
+                        option = scanner.nextInt();
+                    }
+
+                }
+
+                scanner.close();
+
+        }
+        else{
+            logger.log(Level.INFO, "Could not find employee!");
+        }
+
+    }
+
+    public static void employeeOptions(){
+        System.out.println("What would you want to update?");
+        System.out.println("1) Nume\n2) Prenume\n" +
+                "3) Este manager?\n4) Adresa\n" +
+                "5) Numar copii\n6) Salariu\n7) Are permis de conducere?\n" +
+                "0) Iesire\n");
+    }
+
+    public static void options(){
+        System.out.println();
+        System.out.println("1) Afisare angajat dupa ID\n2) Afisare angajati\n" +
+                "3) Afisare angajati dupa nume\n4) Afisare angajati dupa salariu descrescator\n" +
+                "5) Afisare angajati dintr-un departament\n6) Afisare departamente\n7) Afisare categorii job-uri\n" +
+                "8) Stergere angajat\n9) Stergere departament\n10) Stergere categorie job\n" +
+                "11) Actualizare angajat\n12) Actualizare departament\n13) Actualizare categorie job\n" +
+                "0) Iesire\n");
+    }
 }
